b7156ca3e0c3c41705eadbf9678b0b78868e0fbd
xuxiaonan <xuxiaonan@ivymobile.com>
2023-02-04 15:49:12 +0800

    add code

--- p1090/p1090.cpp
+++ p1090/p1090.cpp
@@ -3,6 +3,19 @@ using namespace std;
 int a[10001];
 int main() {
     priority_queue<int, vector<int>, greater<int>> pq;
-    int n;
+    int n, v, ans = 0;
     scanf("%d", &n);
+    for(int i = 0; i < n; ++i) {
+        scanf("%d", &v);
+        pq.push(v);
+    }
+    while(pq.size() > 1) {
+        int a = pq.top();
+        pq.pop();
+        int b = pq.top();
+        pq.pop();
+        ans += a + b;
+        pq.push(a + b);
+    }
+    printf("%d\n", ans);
 }
